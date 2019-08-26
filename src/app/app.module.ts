4d613150d697a92f5b86ba8b2284ae323206d448
Yaramis Naseyb (s103338) <naseyb.yaramis@student.ap.be>
2019-08-26 14:11:11 +0200

    Initialize angular project, changes app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { OvertredingenComponent } from './components/overtredingen/overtredingen.component';
+import { RouterModule } from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { ZoekOvertredingenComponent } from './components/zoek-overtredingen/zoek-overtredingen.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OvertredingenComponent,
+    ZoekOvertredingenComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule, 
+    RouterModule.forChild([
+      {path: '', component: ZoekOvertredingenComponent},
+      {path: 'list', component: OvertredingenComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
